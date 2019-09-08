ac093e0ae4ecc9e3ff5f90bac50d2001bed78f77
Nick Lin <a778977@gmail.com>
2019-09-09 00:29:23 +0800

    check file

--- product.py
+++ product.py
@@ -1,5 +1,13 @@
-#讀取檔案
+import os #operating system 
 products = []
+
+
+if os.path.isfile('products.csv'):#檢查檔案在不在
+    print('椰！找檔案了!')
+else:
+    print('找不到檔案...')
+#讀取檔案
+
 with open ('products.csv', 'r', encoding='utf-8') as f :
     for line in f:
         if '商品,價格' in line:
